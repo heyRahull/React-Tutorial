0ed8d32d7d71ccadaac44bce954a4f5b6ff70f6c
Rahul Agarwal <rahul123agarwal99@gmail.com>
2023-07-30 17:29:27 +0530

    Sample1

---
+++ src/Content.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Content() {
+  return <h1>This is Content</h1>;
+}
+
+export default Content;
---
+++ src/Greeting.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Greeting extends Component {
+  render() {
+    return <div>Greeting</div>;
+  }
+}
+
+export default Greeting;

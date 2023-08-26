3eec6b2f026c0900745fb5fbfa43b2280d1f7e72
Rahul Agarwal <rahul123agarwal99@gmail.com>
2023-08-26 22:49:03 +0530

    video 13 - Form Handling in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from "react";
-import "./styles.css";
-import styles from "./custom.module.css";
+import React, { useState } from "react";
 
 const App = () => {
-  const success = { color: "green" };
+  const [name, setName] = useState("");
+  const [technology, setTechnology] = useState("");
+  const getFormData = (e) => {
+    e.preventDefault();
+  };
+  const handleSubmit = (e) => {
+    console.log(name, technology);
+    setName("");
+    setTechnology("");
+  };
   return (
     <>
-      <h1 style={{ color: "red", backgroundColor: "blue" }}>Inline CSS</h1>
-      <h1 className="primary">External CSS</h1>
-      <h1 className={styles.warn}>Module CSS</h1>
-      <h1 style={success}>Internal CSS</h1>
+      <h1>Form Handling in React</h1>
+      <form action="" onSubmit={getFormData}>
+        Name :{" "}
+        <input
+          type="text"
+          value={name}
+          placeholder="Enter Name"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <br />
+        <br />
+        Technology :{" "}
+        <select
+          value={technology}
+          onChange={(e) => setTechnology(e.target.value)}
+        >
+          <option>Select Option</option>
+          <option>React</option>
+          <option>HTML</option>
+          <option>CSS</option>
+        </select>
+        <br />
+        <br />
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
     </>
   );
 };

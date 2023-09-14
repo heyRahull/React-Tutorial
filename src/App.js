209d3e93320b24ab936f35e6f1c72eb816321cc0
Rahul Agarwal <rahul123agarwal99@gmail.com>
2023-09-14 09:10:24 +0530

    video - 18 componentDidMount Lifecycle Method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
-import User from "./User";
+import React, { Component } from "react";
 
-const App = () => {
-  return (
-    <>
-      <h1>App Component</h1>
-      <User />
-    </>
-  );
-};
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      greet: "Good Morning",
+    };
+  }
+  componentDidMount() {
+    console.warn("componentDidMount");
+  }
+  render(props) {
+    console.warn("render");
+    return (
+      <>
+        <div>User Component</div>
+        <button onClick={() => this.setState({ greet: "Good Evening" })}>
+          Update Message
+        </button>
+        <h1>{this.state.greet}</h1>
+      </>
+    );
+  }
+}
 
 export default App;

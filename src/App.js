16026144010366a64d3f3c0ca34ad7e8543bc319
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-02-17 11:06:19 +0530

    video - 22 what are hooks?

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export class App extends Component {
-  constructor() {
-    super();
-    console.warn("Constructor");
-    this.state = {
-      count: 0,
-    };
-  }
-  componentDidMount() {
-    console.warn("componentDidMount");
-  }
-  componentDidUpdate() {
-    console.warn("componentDidUpdate");
-
-    // this.setState({ greet: "Good Evening" });
-  }
-  shouldComponentUpdate() {
-    console.log("shouldComponentUpdate", this.state.count);
-    if (this.state.count < 5) return true;
-    return false;
-  }
-  render() {
-    console.warn("Render");
-    return (
-      <>
-        <h1>shouldComponentUpdate Lifecycle Method</h1>
-        <h1>{this.state.count}</h1>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Update Count
-        </button>
-      </>
-    );
-  }
-}
+const App = () => {
+  const [data, setData] = useState("The Humble");
+  return (
+    <>
+      <h2>{data}</h2>
+      <button
+        onClick={() => {
+          setData("Coder");
+        }}
+      >
+        Update
+      </button>
+    </>
+  );
+};
 
 export default App;

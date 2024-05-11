5b8802ed1a73572462f924a74b9c4b5676650922
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-05-12 00:31:41 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import "./App.css";
-import ThemeSwitcher from "./ThemeSwitcher";
-import { ThemeProvider } from "./ThemeContext";
-
 export default function App() {
   return (
     <div className="App">

b7415c0e3beb607363b3d241eba470ca5f4f9425
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-05-12 00:54:11 +0530

    React Live Streaming Project with ZegoCloud

---
+++ src/Room.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import { useParams } from "react-router-dom";
+
+const Room = () => {
+  const { roomID } = useParams();
+  function randomID(len) {
+    let result = "";
+    if (result) return result;
+    var chars =
+        "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
+      maxPos = chars.length,
+      i;
+    len = len || 5;
+    for (i = 0; i < len; i++) {
+      result += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return result;
+  }
+
+  let myMeeting = async (element) => {
+    // generate Kit Token
+    const appID = //Enter your app id here ;
+    const serverSecret = //Enter your secret tiken here ;
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      randomID(5),
+      randomID(5)
+    );
+
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Copy link",
+          url:
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname +
+            "?roomID=" +
+            roomID,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.LiveStreaming, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
+  };
+
+  return <div ref={myMeeting}></div>;
+};
+
+export default Room;
